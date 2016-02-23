25bfe1c340c6d3657908d4c16c71d96f9bc4484f
AmberVandeKerkhoff <amber.l.vandy@gmail.com>
2016-02-23 17:13:30 -0500

    Trips index map done

--- public/js/tripIndex.js
+++ public/js/tripIndex.js
@@ -1,1 +1,45 @@
 console.log('TRIP INDEX works!');
+
+$(function() {
+
+// Google Map Settings - create variable for the map
+	var intializeMap = function () {
+
+		// create variable for new map
+		var map = new google.maps.Map(document.getElementById('mapImg'), {
+		    zoom: 1,
+		    minZoom: 0,
+		    center: new google.maps.LatLng(24.529795, 4.206753),
+		    mapTypeId: google.maps.MapTypeId.ROADMAP
+		});
+		
+		// Add new markers
+		addMarkers(map);
+
+	} // end initialize
+
+	// Display the map
+	google.maps.event.addDomListener(window, 'load', intializeMap);
+
+
+}); // <-------------------------------- END WINDOW.ONLOAD
+
+// add markers function
+var addMarkers = function(map) {
+	// ajax call to get lat and lng from 'trips/json' route
+	$.ajax('/trips/json')
+	.done(function(result) {
+		// console.log(result);
+		// need for loop to iterate through all trips
+		for (var i = 0; i < result.length; i++) {
+			var marker = new google.maps.Marker ({
+			    map: map,
+			    icon: '/images/marker.png',
+			    position: { lat: result[i].lat, lng: result[i].lng },
+			    title: result[i].destination
+			});
+		};
+	});
+} // end addMarkers function
+
+
